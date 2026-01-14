ec5142515fcd96e78e3a21425ca1cc91c3cd13b1
Nistal Talson <nistaltt@gmail.com>
2026-01-14 21:10:25 +0530

    oddoreven

---
+++ oddoreven.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n%2==0)
+        printf("the number entered is even");
+    else 
+        printf("number is odd");
+    return 0;
+
+}
